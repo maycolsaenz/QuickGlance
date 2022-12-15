15615952e14966da85705363f31a06cb999e413f
maycolsj <maycol.saenz@ucr.ac.cr>
2022-12-14 21:53:39 -0600

    Learning how to use Tkinter

--- QuickGlanceV1.0/main.py
+++ QuickGlanceV1.0/main.py
@@ -2,4 +2,10 @@
 
 from tkinter import*
 root = Tk()
+# %% Creating a Label Widget
+myLabel = Label(root, text = 'Hello World!')
+#Shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
 # %%
