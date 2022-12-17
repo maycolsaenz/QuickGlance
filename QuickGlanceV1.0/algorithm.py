3efd82a9bdd129b5d9e2e8f3a3aebe818ca98dbe
maycolsj <maycol.saenz@ucr.ac.cr>
2022-12-17 11:48:12 -0600

    Time bar 5s up to 30s, steps 5

--- QuickGlanceV1.0/algorithm.py
+++ QuickGlanceV1.0/algorithm.py
@@ -2,16 +2,16 @@
 import os
 import random
 import tkinter
-import time
+
 #%% Graphic User interphase definition
 window = tkinter.Tk()
 # This places the main window on top of other programs
 window.attributes('-topmost',True)
-window.geometry("700x100")
+window.geometry("700x80")
 window.title("QuickGlance")
 window.configure(bg='#DDE4EA')
 # Time variable (ms)
-time_delay = 3000
+
 
 # OOP algorithm
 class base:
@@ -21,22 +21,37 @@ class base:
         self.__lines = 0
         self.__randomList = []
         self.__lineDisplayed = []
+
         self.MainLabel = tkinter.Label(window, 
-        text = "This is a test",
-        bg = '#EAC950',
-        font=("Helvetica", 20),
-        #fg = '#EAC950' This sets font color
-        )
+            text = "This is a test",
+            bg = '#EAC950',
+            font=("Helvetica", 14),)
         self.MainLabel.pack(side="top")  
+
+        self.bar = tkinter.Scale(window,
+            from_=5,
+            resolution=5,
+            to=30,
+            orient='horizontal',
+            bg = '#E36476')
+        self.bar.pack(side="top") 
+        #self.bar.grid(row = 2, column = 1)
+        
 
     def update_label(self,x=0):
         #Stop condition
         if x < self.lines:
             self.MainLabel.config(text = self.lineDisplayed[x])
+            user_delay = self.bar.get()
+            time_delay = 1000*user_delay
             x+=1
-        else: x=0
+            # Updates x value at the end of the list is displayed
+            if x<self.lines:
+                pass
+            else:
+                x = 0
         window.after(time_delay, lambda: self.update_label(x))
- 
+
     #__fileObject getter 
     @property
     def fileObject(self):
@@ -84,13 +99,12 @@ class base:
         for n in self.randomList:
             output.append(reading[n].strip())
         self.lineDisplayed = output
-    
-
 
 ################################################################
+
 #%% File name to be read
 fileName = 'file.txt'
-fileObjectX = open(fileName, 'r')
+fileObjectX = open(fileName, 'r', encoding="utf-8")
 Main_object = base(fileObjectX)
 
 # Counts the amount of lines in the file
@@ -100,7 +114,8 @@ Main_object.randomList()
 # Displays random lines
 Main_object.displayLine()
 #This function changes the label phrase
-window.after(time_delay, lambda: Main_object.update_label())
+Main_object.update_label()
+
 # Testing
 print(Main_object.lineDisplayed)
 #%%
